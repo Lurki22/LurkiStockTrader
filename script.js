a1823bbcffdcca8c37bb10574cb1cfbd6a5f2356
Lurki22 <grafgorroff@outlook.com>
2025-03-13 20:52:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// script.js
 let stockData = [];
 const ctx = document.getElementById('chart').getContext('2d');
+const timeRangeSelect = document.getElementById('timeRange');
 
 const chart = new Chart(ctx, {
     type: 'line',
@@ -24,13 +26,23 @@ const chart = new Chart(ctx, {
 function updateStockData() {
     const newPrice = (Math.random() * 100 + 50).toFixed(2);
     const time = new Date().toLocaleTimeString();
-
+    
     stockData.push({ time, price: newPrice });
-    if (stockData.length > 20) stockData.shift();
-
+    if (stockData.length > 50) stockData.shift();
+    
     chart.data.labels = stockData.map(d => d.time);
     chart.data.datasets[0].data = stockData.map(d => d.price);
     chart.update();
 }
 
 setInterval(updateStockData, 2000);
+
+timeRangeSelect.addEventListener('change', function() {
+    let timeRange = timeRangeSelect.value;
+    stockData = [];
+    chart.data.labels = [];
+    chart.data.datasets[0].data = [];
+    chart.update();
+
+    console.log("Zeitraum geändert zu: ", timeRange);
+});
